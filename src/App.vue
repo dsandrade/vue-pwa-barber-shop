937ed1e642cd1627c4c106eaa7017a01fde68eab
Daniel Andrade <dsandrade777@gmail.com>
2019-02-14 01:19:09 -0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div class="content">
+      <!--<header>-->
+      <!--<span>Vue.js PWA</span>-->
+      <!--</header>-->
+      <main>
+        <img
+          class="login-logo"
+          src="http://192.168.15.234:8080/static/img/logo.png" alt="BarberShop PWA"
+        >
+        <router-view></router-view>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  .login-logo {
+    width: 100%;
+    max-width: 350px;
+  }
+
+  html {
+    background: url(http://192.168.15.234:8080/static/img/login.png) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    background-size: cover;
+    -o-background-size: cover;
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  main {
+    text-align: center;
+  }
+
+  header {
+    margin: 0;
+    height: 56px;
+    padding: 0 16px 0 24px;
+    background-color: #35495E;
+    color: #ffffff;
+  }
+
+  header span {
+    display: block;
+    position: relative;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: .02em;
+    font-weight: 400;
+    box-sizing: border-box;
+    padding-top: 16px;
+  }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="hello" style="display: flex; flex-flow: column wrap; align-items: center;">
+    <div>
+      <input
+        type="text"
+        v-model="login"
+        :placeholder="loginPlaceholder"
+        @focus="loginPlaceholder = ''"
+        @blur="loginPlaceholder = 'Digite aqui o seu usuário'"
+        class="input-login-form"
+      >
+      <input
+        type="password"
+        v-model="password"
+        :placeholder="passwordPlaceholder"
+        @focus="passwordPlaceholder = ''"
+        @blur="passwordPlaceholder = 'Digite aqui o seu usuário'"
+        class="input-login-form"
+        style="margin-top: 3%;"
+      >
+      <button
+        type="submit"
+        class="button-login-form"
+        style="margin-top: 3%;"
+      >LOG IN</button>
+    </div>
+    <div style="text-align: center; margin-top: 4%; margin-bottom: 4%;">
+      <span class="span-items">
+        Esqueceu a senha? | <span>Criar nova conta?</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        loginPlaceholder: 'Digite aqui o seu usuário',
+        passwordPlaceholder: 'Digite aqui sua senha',
+        login: '',
+        password: '',
+        msg: 'Welcome to Your Vue.js PWA'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .span-items {
+    font-size: 18px;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .span-items span {
+    color: #E57953;
+  }
+
+  .input-login-form {
+    padding: 6% 20%;
+    text-align: center;
+    border-radius: 100px;
+    background-color: rgba(255, 255, 255, 0.2);
+    border: 1px solid transparent;
+    color: #fff;
+    cursor: text;
+  }
+
+  .button-login-form {
+    padding: 5% 35%;
+    text-align: center;
+    border-radius: 50px;
+    border: 0px;
+    background-color: #fff;
+    color: #E57953;
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+  .input-login-form::placeholder {
+    color: #fff;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #35495E;
+  }
+</style>
